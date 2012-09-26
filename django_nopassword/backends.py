431e591afc883e1f453d070d7ec18991fdfec512
Rolf Erik Lekang <relekang@gmail.com>
2012-09-26 19:39:05 +0200

    implements backend

--- django_nopassword/backends.py
+++ django_nopassword/backends.py
@@ -1,13 +1,30 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
 from django.contrib.auth.models import User
 from django_nopassword.models import LoginCode
 
+#Todo: move to settings
+TIMEOUT = timedelta(minutes=15)
+
 class EmailBackend:
     
-    def authenticate(self, username, password=None):
+    def authenticate(self, username, code=None):
         try:
             user = User.objects.get(username=username)
-            user = LoginCode.check(user, password)
-            return user
+            if code is None:
+                return LoginCode.create_code_for_user(user)
+            else:
+                timestamp = datetime.now() + TIMEOUT
+                login_code = LoginCode.objects.get(user=user, code=code, timestamp__lt=timestamp)
+                user = login_code.user
+                user.code = login_code
+                login_code.delete()
+                return user
         except (TypeError, User.DoesNotExist):
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
             return None

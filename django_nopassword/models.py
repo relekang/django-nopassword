57d2c6122725fc943ec19e39ed1fcd678e548d00
Rolf Erik Lekang <relekang@gmail.com>
2012-09-26 19:40:06 +0200

    makes models send mail and generate code

--- django_nopassword/models.py
+++ django_nopassword/models.py
@@ -1,26 +1,50 @@
 # -*- coding: utf-8 -*-
+from random import choice
+import string
+from django.conf import settings
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 from django.db import models
-from datetime import datetime, timedelta
+from datetime import datetime
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-
-#Todo: move to settings
-TIMEOUT = timedelta(minutes=15)
 
 class LoginCode (models.Model):
     user = models.ForeignKey(User, related_name='login_codes', editable=False, verbose_name=_('user'))
     code = models.CharField(max_length=2, editable=False, verbose_name=_('code'))
     timestamp = models.DateTimeField(editable=False)
+    next = models.TextField(editable=False, blank=True)
 
     def __unicode__ (self):
         return "%s - %s" % (self.user, self.timestamp)
 
-    @classmethod
-    def check (cls, user, code):
-        try:
-            timestamp = datetime.now() + TIMEOUT
-            login_code = cls.objects.get(user=user, code=code, timestamp__gt=timestamp)
-            return user
-        except (TypeError, cls.DoesNotExist):
-            return None
+    def save(self, *args, **kwargs):
+        self.timestamp = datetime.now()
+        if not self.next:
+            self.next = '/'
+        super(LoginCode, self).save(*args, **kwargs)
+        send_mail(
+            'Login code',
+            'Login with this url %s%s?next=%s' % (
+                settings.SERVER_URL,
+                reverse('django_nopassword.views.login_with_code', args=[self.user.username, self.code]),
+                self.next
+            ),
+            settings.SERVER_EMAIL,
+            [self.user.email],
+        )
 
+
+    @classmethod
+    def create_code_for_user(cls, user, next=None):
+        code = cls.generate_code()
+        login_code = LoginCode(user=user, code=code)
+        if not next is None:
+            login_code.next = next
+        login_code.save()
+        return login_code
+
+    @classmethod
+    def generate_code(cls, length=20):
+        chars = string.letters + string.digits
+        return ''.join([choice(chars) for i in xrange(length)])

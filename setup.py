62631f7301630d5d3bee459fc203f65d40e070ec
Rolf Erik Lekang <relekang@gmail.com>
2013-05-09 11:48:49 +0200

    remove lond description from setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 setup(
     name="django-nopassword",
-    version='0.3',
+    version='0.3.1',
     url='http://github.com/relekang/django-nopassword',
     author='Rolf Erik Lekang',
     author_email='me@rolflekang.com',
     description='Authentication backend for django that uses email verification instead of passwords',
-    long_description=open('README.md').read(),
     packages=find_packages(exclude='tests'),
     tests_require=[
         'django>=1.4',

3e65aa7c6559cd69891530cba222d70e8486b5d5
Rolf Erik Lekang <me@rolflekang.com>
2014-01-13 12:02:42 -0800

    bump version number to 0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 setup(
     name="django-nopassword",
-    version='0.5.0',
+    version='0.6.0',
     url='http://github.com/relekang/django-nopassword',
     author='Rolf Erik Lekang',
     author_email='me@rolflekang.com',

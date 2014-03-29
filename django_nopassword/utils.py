3ce17861a6891650e3c37d368b3ccef4e6da7ca1
Rolf Erik Lekang <me@rolflekang.com>
2014-03-29 14:20:49 -0700

    use AUTH_USER_MODULE instead of get_user_model()
    This will remove some import loops

--- django_nopassword/utils.py
+++ django_nopassword/utils.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import django
+from django.conf import settings
 
 if django.VERSION >= (1, 5):
     from django.contrib.auth import get_user_model
     User = get_user_model()
+    AUTH_USER_MODULE = settings.AUTH_USER_MODULE
 else:
     from django.contrib.auth.models import User
+    AUTH_USER_MODULE = 'auth.User'
 
 
 USERNAME_FIELD = getattr(User, 'USERNAME_FIELD', 'username')

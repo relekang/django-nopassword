4be587a71796e365efd9c53d7add23a5ce612bf5
Rolf Erik Lekang <me@rolflekang.com>
2014-04-01 09:24:33 -0700

    add license to setup.py

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ setup(
     url='http://github.com/relekang/django-nopassword',
     author='Rolf Erik Lekang',
     author_email='me@rolflekang.com',
-    description='Authentication backend for django that uses email verification instead of passwords',
+    description='Authentication backend for django that uses '
+                'email verification instead of passwords',
     packages=find_packages(exclude='tests'),
     tests_require=[
         'django>=1.4',
     ],
+    license='MIT',
     test_suite='runtests.runtests',
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Django",

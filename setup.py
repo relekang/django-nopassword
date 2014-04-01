8216f044a77afe7398c5dee0cac0b18315d6331d
Rolf Erik Lekang <me@rolflekang.com>
2014-04-01 09:17:31 -0700

    add python version classifiers

--- setup.py
+++ setup.py
@@ -18,6 +18,10 @@ setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Django",
         "Environment :: Web Environment",

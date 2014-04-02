5b6507ac72914b3846c7a419bc79eaf65b04ce2b
Rolf Erik Lekang <me@rolflekang.com>
2014-04-01 23:01:18 -0700

    refactor runtests file
    it will now only run django-nopasswords tests

--- runtests.py
+++ runtests.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python
+import os
 import sys
-from optparse import OptionParser
-from os.path import abspath, dirname
-from django.test.simple import DjangoTestSuiteRunner
-from django.test.utils import setup_test_environment
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+from django.test.utils import get_runner
+from django.conf import settings
+
+TestRunner = get_runner(settings)
 
 
-def runtests(*test_args, **kwargs):
-    # setup_test_environment()
-    parent = dirname(abspath(__file__))
-    sys.path.insert(0, parent)
-    test_runner = DjangoTestSuiteRunner(
-        verbosity=kwargs.get('verbosity', 1),
-        interactive=kwargs.get('interactive', False),
-        failfast=kwargs.get('failfast')
-    )
-    failures = test_runner.run_tests(test_args)
-    sys.exit(failures)
+def runtests():
+    test_runner = TestRunner(verbosity=1, interactive=True)
+    failures = test_runner.run_tests(['tests'])
+    sys.exit(bool(failures))
+
 
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option('--failfast', action='store_true', default=False, dest='failfast')
-
-    (options, args) = parser.parse_args()
-
-    runtests(failfast=options.failfast, *args)
+    runtests()

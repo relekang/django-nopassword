7e28a460749448d3c57c7283f81fb45112908e86
Rolf Erik Lekang <me@rolflekang.com>
2014-05-10 19:41:27 -0700

    fix django 1.7 compatibility in test_runner

--- runtests.py
+++ runtests.py
@@ -6,11 +6,13 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 from django.test.utils import get_runner
 from django.conf import settings
+import django
 
-TestRunner = get_runner(settings)
+django.setup()
 
 
 def runtests():
+    TestRunner = get_runner(settings)
     test_runner = TestRunner(verbosity=1, interactive=True)
     failures = test_runner.run_tests(['tests'])
     sys.exit(bool(failures))

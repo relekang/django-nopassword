03b3ce511293c0c1cf9991795a8116069e126282
Rolf Erik Lekang <me@rolflekang.com>
2014-10-02 09:29:28 +0200

    1.0.0 :sparkles:

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 setup(
     name="django-nopassword",
-    version='0.8.0',
+    version='1.0.0',
     url='http://github.com/relekang/django-nopassword',
     author='Rolf Erik Lekang',
     author_email='me@rolflekang.com',

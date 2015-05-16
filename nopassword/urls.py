5e957853fcd7368ddf9d5fdebe812b7605dd1f35
Rolf Erik Lekang <me@rolflekang.com>
2015-05-16 10:19:20 +0200

    Remove deprecated patterns

--- nopassword/urls.py
+++ nopassword/urls.py
@@ -1,12 +1,11 @@
 # -*- coding: utf8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^login/$', 'nopassword.views.login', name='login'),
     url(r'^login-code/(?P<login_code>[a-zA-Z0-9]+)/$',
         'nopassword.views.login_with_code'),
     url(r'^login-code/(?P<username>[a-zA-Z0-9_@\.-]+)/(?P<login_code>[a-zA-Z0-9]+)/$',
         'nopassword.views.login_with_code_and_username'),
     url(r'^logout/$', 'nopassword.views.logout'),
-)
+]

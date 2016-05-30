e21ecfea713350132a9ccb9bcb7127f59e1a1ac2
daronstinnett <darons@sequoialane.com>
2016-05-29 23:14:08 -0700

    fix: RemovedInDjango110Warnings in urls.py (#66)

--- nopassword/urls.py
+++ nopassword/urls.py
@@ -1,11 +1,13 @@
 # -*- coding: utf8 -*-
 from django.conf.urls import url
+from nopassword.views import login, login_with_code, \
+    login_with_code_and_username, logout
+
 
 urlpatterns = [
-    url(r'^login/$', 'nopassword.views.login', name='login'),
-    url(r'^login-code/(?P<login_code>[a-zA-Z0-9]+)/$',
-        'nopassword.views.login_with_code'),
+    url(r'^login/$', login, name='login'),
+    url(r'^login-code/(?P<login_code>[a-zA-Z0-9]+)/$', login_with_code),
     url(r'^login-code/(?P<username>[a-zA-Z0-9_@\.\+-]+)/(?P<login_code>[a-zA-Z0-9]+)/$',
-        'nopassword.views.login_with_code_and_username'),
-    url(r'^logout/$', 'nopassword.views.logout'),
+        login_with_code_and_username),
+    url(r'^logout/$', logout),
 ]
